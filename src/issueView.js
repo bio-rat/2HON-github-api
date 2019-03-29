fa2c22e01276556b02ead6427e197f8ce3d7ef2c
tonggianghuy <47965892+tonggianghuy@users.noreply.github.com>
2019-03-29 17:50:28 +0700

    add markdown

--- src/issueView.js
+++ src/issueView.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import "./issueView.css";
+import ReactMarkdown from "react-markdown";
 
 export default class issueView extends Component {
   render() {
     return (
       <Container>
         <Row className="issueBox">
-          <Col xs="10" className="issueContent text-left py-4">
+          <Col xs="10" className="issueContent text-left py-4 pl-4 pr-0">
             <div>
               <h3>
                 #{this.props.number} {this.props.title}
               </h3>
             </div>
-            <div>`{this.props.body}`</div>
+            <div>
+              <ReactMarkdown source={this.props.body} />
+            </div>
           </Col>
-          <Col xs="2" className="issueUser text-right py-4">
+          <Col xs="2" className="issueUser text-right py-4 pr-4 pl-0">
             <img src={this.props.user.avatar_url} className=" border rounded" />
             <h6>@{this.props.user.login}</h6>
           </Col>
