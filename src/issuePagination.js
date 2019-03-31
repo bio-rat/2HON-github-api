7d82941d04be9cd4f56cb0822c716f00b11237c0
etudeofmemories8698 <etudeofmemories8698@gmail.com>
2019-03-31 18:55:16 +0700

    add pagination (#33)

--- src/issuePagination.js
+++ src/issuePagination.js
@@ -4,17 +4,13 @@ import ReactDOM from "react-dom";
 import PaginationComponent from "react-reactstrap-pagination";
 
 export default class Example extends React.Component {
-  handleSelected(selectedPage) {
-    console.log("selected", selectedPage);
-    this.setState({ selectedPage: selectedPage });
-  }
   render() {
     return (
       <div className="pagination-wrapper">
         <PaginationComponent
-          totalItems={30}
-          pageSize={3}
-          onSelect={this.handleSelected}
+          totalItems={10}
+          pageSize={1}
+          onSelect={selectedPage => this.props.onSelect(selectedPage)}
         />
       </div>
     );
